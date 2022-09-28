177fc5d79d77a5359c55237d9f79cab1bf92b14c
Yasir Yousuf Rimon <81034032+rimonyousuf@users.noreply.github.com>
2022-09-28 06:50:29 +0600

    Updated

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ class otp_verification:
         self.root.geometry("600x500+300+70")
         self.root.resizable(False,False)
 
+        self.user=random.randint(1000,9999)
+        self.client=Client("SS ID","Auth Token")
+        self.client.messages.create(to=["+880##########"],
+                                    from_="your twilio number",
+                                    body=self.user)
+
         self.can=Canvas(self.root,bg='white',width=400,height=280)
         self.can.place(x=100,y=60)
 
@@ -22,8 +28,30 @@ class otp_verification:
         self.btn_submit=Button(self.root,text='Submit',font=('times new roman',17,'bold'),bg='#ffebcd',fg='black')
         self.btn_submit.place(x=245,y=220)
 
-        self.btn_resend=Button(self.root,text='Resend OTP',font=('times new roman',17,'bold'),bg='#e3cf57',fg='black')
+        self.btn_resend=Button(self.root,text='Resend OTP',command=self.resend_otp,font=('times new roman',17,'bold'),bg='#e3cf57',fg='black')
         self.btn_resend.place(x=217,y=400)
+
+
+    def check_otp(self):
+        try:
+            self.userInput=int(self.entry.get(1.0,"end-1c"))
+            if self.userInput==self.user:
+                messagebox.showinfo("Success","Login successs")
+                self.user="done"
+            elif self.user=="done":
+                messagebox.showinfo("Already","Already login")
+            else:
+                 messagebox.showerror("Error","Wrong OTP")
+        except:
+            messagebox.showerror("Error","Invalid OTP")
+
+
+    def resend_otp(self):
+        self.user=random.randint(1000,9999)
+        self.client=Client("SS ID","Auth Token")
+        self.client.messages.create(to=["+880##########"],
+                                    from_="your twilio number",
+                                    body=self.user)
 
 
 root=Tk()
